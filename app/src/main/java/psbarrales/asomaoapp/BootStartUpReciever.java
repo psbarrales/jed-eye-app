bca5dd2d9de284873cfef99dc04fccbdc48b1821
Pablo Barrales <psbarrales@gmail.com>
2014-09-14 14:58:11 -0300

    V1.0

---
+++ app/src/main/java/psbarrales/asomaoapp/BootStartUpReciever.java
@@ -0,0 +1,18 @@
+package psbarrales.asomaoapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootStartUpReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // Start Service On Boot Start Up
+        Intent service = new Intent(context, Asomao.class);
+        context.startService(service);
+
+
+    }
+}
